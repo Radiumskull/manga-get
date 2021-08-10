981aa96992fbf2df85684eae9448f97bc9e7de4d
Aritra Bhattacharjee <whoisaritra@gmail.com>
2021-08-10 10:37:26 +0530

    Issue Fixed where it worked on in project root directory

--- utils/downloader.go
+++ utils/downloader.go
@@ -1,49 +1,55 @@
 package utils
 
 import (
-    "fmt"
-    "os"
-    "net/http"
-    "io"
-    "strings"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
 )
 
-
-func check(e error){
-    if(e != nil){
-        panic(e);
-    }
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
 
 func CreateDir(path string) {
-    _, err := os.Stat(path)
-    if err == nil { return }
-    mkErr := os.Mkdir(path, 0755)
-    if mkErr != nil {
-        check(mkErr)
-    }
+	pwd, _ := os.Getwd()
+	currDir := pwd + "/"
+
+	_, err := os.Stat(currDir + path)
+	if err == nil {
+		return
+	}
+	mkErr := os.Mkdir(currDir+path, 0755)
+	if mkErr != nil {
+		check(mkErr)
+	}
 }
 
 func FileNameParse(url string) (string, error) {
-    components := strings.Split(url, "/")
-    n := len(components)
-    fileName := strings.Replace(components[n-1], "_", " ", -1)
-    //fmt.Printf("%s\n", fileName)
-    return fileName, nil
+	components := strings.Split(url, "/")
+	n := len(components)
+	fileName := strings.Replace(components[n-1], "_", " ", -1)
+	//fmt.Printf("%s\n", fileName)
+	return fileName, nil
 }
 
 func DownloadFileHandler(dirName string, url string) {
-    resp, _ := http.Get(url)
+	pwd, _ := os.Getwd()
+	currDir := pwd + "/"
 
-    defer resp.Body.Close();
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
 
-    fileName, err := FileNameParse(url)
-    check(err)
-    filePath := "./Manga/" + dirName + "/" + fileName + ".cbr"
-    out, _ := os.Create(filePath)
+	fileName, err := FileNameParse(url)
+	check(err)
+	filePath := currDir + "Manga/" + dirName + "/" + fileName + ".cbr"
+	out, _ := os.Create(filePath)
 
-    defer out.Close();
+	defer out.Close()
 
-    _, _ = io.Copy(out, resp.Body);
-    fmt.Printf("%s Downloaded\n", fileName)
+	_, _ = io.Copy(out, resp.Body)
+	fmt.Printf("%s Downloaded\n", fileName)
 }
--- utils/scraper.go
+++ utils/scraper.go
@@ -1,123 +1,126 @@
 package utils
 
 import (
-    "fmt"
-    "github.com/gocolly/colly"
-    "database/sql"
-    "strconv"
+	"database/sql"
+	"fmt"
+	"os/user"
+	"strconv"
 
-    _ "github.com/mattn/go-sqlite3"
+	"github.com/gocolly/colly"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var (
-    baseURL = "https://w12.mangafreak.net/"
-    libraryURL = "https://w12.mangafreak.net/Mangalist/All/"
+	baseURL    = "https://w12.mangafreak.net/"
+	libraryURL = "https://w12.mangafreak.net/Mangalist/All/"
+	dirPath    = "/Documents/mangafreak.db"
+	osPath     string
 )
 
-func updateMangaListHandler(page int){
-    database, _ := sql.Open("sqlite3", "./data/mangafreak.db")
-    insert, _ := database.Prepare("INSERT INTO Manga(title, url) VALUES(?, ?)")
+func updateMangaListHandler(page int) {
+	user, _ := user.Current()
+	database, _ := sql.Open("sqlite3", user.HomeDir+"/"+dirPath)
 
-    c := colly.NewCollector()
+	insert, _ := database.Prepare("INSERT INTO Manga(title, url) VALUES(?, ?)")
 
+	c := colly.NewCollector()
 
-    c.OnHTML(".list_item > .list_item_info > h3 > a[href]", func(e *colly.HTMLElement) {
-        link := e.Attr("href")
-        //fmt.Println(reflect.TypeOf(e.Text), reflect.TypeOf(link))
-        insert.Exec(e.Text, link);
-    })
+	c.OnHTML(".list_item > .list_item_info > h3 > a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		//fmt.Println(reflect.TypeOf(e.Text), reflect.TypeOf(link))
+		insert.Exec(e.Text, link)
+	})
 
-    c.OnScraped(func (r *colly.Response){
-        fmt.Printf("Finished Scraping Page %d\n", page)
-        //wg.Done()
-    })
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Printf("Finished Scraping Page %d\n", page)
+		//wg.Done()
+	})
 
-    c.OnError(func(r *colly.Response, err error){
-        fmt.Println("Request URL:", r.Request.URL, "\nError:", err)
-    })
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "\nError:", err)
+	})
 
-
-    c.Visit(libraryURL + strconv.Itoa(page))
+	c.Visit(libraryURL + strconv.Itoa(page))
 }
-
 
 // This Function iterates through all the pages of mangafreak website and lists all the manga and updates mangafreak.db file
-func UpdateMangaList(){
-    //var wg sync.WaitGroup
-    database, _ := sql.Open("sqlite3", "./data/mangafreak.db")
+func UpdateMangaList() {
+	user, _ := user.Current()
+	database, _ := sql.Open("sqlite3", user.HomeDir+"/"+dirPath)
 
-    createTable, _ := database.Prepare("CREATE TABLE IF NOT EXISTS Manga(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, url TEXT)");
-    dropTable, _ := database.Prepare("DROP TABLE IF EXISTS Manga");
+	createTable, _ := database.Prepare("CREATE TABLE IF NOT EXISTS Manga(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, url TEXT)")
+	dropTable, _ := database.Prepare("DROP TABLE IF EXISTS Manga")
 
-    dropTable.Exec();
-    createTable.Exec();
-    for page := 1; page <= 309; page++ {
-        //wg.Add(1)
-        updateMangaListHandler(page);
-    }
+	dropTable.Exec()
+	createTable.Exec()
+	for page := 1; page <= 309; page++ {
+		//wg.Add(1)
+		updateMangaListHandler(page)
+	}
 
-    //wg.Wait()
+	//wg.Wait()
 }
 
-func FetchMangaPageLink(id int) (string, string, error){
-    database, _ := sql.Open("sqlite3", "./data/mangafreak.db")
+func FetchMangaPageLink(id int) (string, string, error) {
+	user, _ := user.Current()
+	database, _ := sql.Open("sqlite3", user.HomeDir+"/"+dirPath)
 
-    fetchRow, _ := database.Prepare("SELECT title, url FROM Manga WHERE id = ?")
-    row := fetchRow.QueryRow(id)
+	fetchRow, _ := database.Prepare("SELECT title, url FROM Manga WHERE id = ?")
+	row := fetchRow.QueryRow(id)
 
-    var (
-        title string
-        url string
-    )
+	var (
+		title string
+		url   string
+	)
 
-    row.Scan(&title, &url)
-    return title, url, nil
-
+	row.Scan(&title, &url)
+	return title, url, nil
 }
 
 func SyncManga(title string, url string) {
 
-    c := colly.NewCollector()
+	c := colly.NewCollector()
 
-    c.OnRequest(func (r *colly.Request) {
-        fmt.Println("Visiting", r.URL.String())
-    })
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
 
-    c.OnScraped(func (r *colly.Response){
-        fmt.Println("Finished", r.Request.URL)
-    })
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
+	})
 
-    c.OnHTML(".manga_series_list > table > tbody > tr > td:last-child > a", func(e *colly.HTMLElement) {
-        link := e.Attr("href")
+	c.OnHTML(".manga_series_list > table > tbody > tr > td:last-child > a", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
 
-        DownloadFileHandler(title, baseURL + link)
-    })
+		DownloadFileHandler(title, baseURL+link)
+	})
 
-    c.Visit(baseURL + url)
+	c.Visit(baseURL + url)
 }
 
-func FetchFromDatabase(name string){
-    database, dbErr := sql.Open("sqlite3", "./data/mangafreak.db")
-    if dbErr != nil {
-        fmt.Println("Database Not Found.")
-        panic(dbErr)
-    }
-    searchTable, _ := database.Prepare("SELECT id, title, url FROM Manga WHERE title LIKE ?")
-    rows, queryErr := searchTable.Query("%" + name + "%")
-    if queryErr != nil {
-        fmt.Println("Query Err")
-        panic(queryErr)
-    }
+func FetchFromDatabase(name string) {
+	user, _ := user.Current()
+	database, dbErr := sql.Open("sqlite3", user.HomeDir+"/"+dirPath)
+	if dbErr != nil {
+		fmt.Println("Database Not Found.")
+		panic(dbErr)
+	}
+	searchTable, _ := database.Prepare("SELECT id, title, url FROM Manga WHERE title LIKE ?")
+	rows, queryErr := searchTable.Query("%" + name + "%")
+	if queryErr != nil {
+		fmt.Println("Query Err")
+		panic(queryErr)
+	}
 
-    for rows.Next(){
-        var (
-            id int
-            title string
-            url string
-        )
-        rows.Scan(&id, &title, &url)
-        fmt.Printf("%d : %s\n", id, title)
-    }
-    fmt.Println("Records Ended.")
+	for rows.Next() {
+		var (
+			id    int
+			title string
+			url   string
+		)
+		rows.Scan(&id, &title, &url)
+		fmt.Printf("%d : %s\n", id, title)
+	}
+	fmt.Println("Records Ended.")
 }
-
